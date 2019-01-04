538ca16da025847f2ef712cc9a396fc9014eddc2
michaelravedoni <michael.ravedoni@gmail.com>
2019-01-04 16:05:09 +0100

    up: github-deploy
    # Conflicts:
    #	bin/github-deploy.sh

--- bin/github-deploy.sh
+++ bin/github-deploy.sh
@@ -2,6 +2,9 @@
 
 # Change this if you change the output folder in config.yml
 BUILD_DIR="_site"
+DEPLOY_DIR="_deploy"
+BRANCH="gh-pages"
+TEMP=$(mktemp -d -t jgd-XXX)
 
 DIR=$(dirname "$0")
 cd $DIR/..
@@ -12,14 +15,28 @@ cd $DIR/..
 #     exit 1;
 # fi
 
-echo "Deleting old publication"
-rm -rf $BUILD_DIR
-mkdir $BUILD_DIR
+echo "Delete and empty ${BUILD_DIR} and ${DEPLOY_DIR} old directories"
+rm -rf ${BUILD_DIR}
+rm -rf ${DEPLOY_DIR}
+mkdir ${BUILD_DIR}
+mkdir ${DEPLOY_DIR}
 
-echo "Generating site"
+echo "Generating jekyll site with _config-github.yml config file"
 # hugo --config=config.yml,config/environments/github.yml
 bundle exec jekyll build --config _config.yml,_config-github.yml
 
-echo "Updating gh-pages branch"
-git add -f $BUILD_DIR && git commit -m "Automated deploy at $(date)."
-git subtree push --prefix $BUILD_DIR origin gh-pages
+echo "Copy ${BUILD_DIR} to ${DEPLOY_DIR}"
+cp -r ${BUILD_DIR}/. ${DEPLOY_DIR}
+
+echo "Deploy into ${BRANCH} branch"
+cd ${DEPLOY_DIR}
+#git checkout --orphan $BRANCH
+#rm -rf *
+#cp -r ${DEPLOY_DIR}/ .
+git add . --dry-run
+git commit -am "Automated deploy at $(date)" --allow-empty --dry-run
+#git push origin $BRANCH --dry-run
+
+
+echo -e "Cleaning up"
+rm -rf $DEPLOY_DIR
